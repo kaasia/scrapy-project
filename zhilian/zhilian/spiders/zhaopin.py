4c91704db4e943e3b8cff0bfb1fa6a2fc487db29
kaasia <goodboyasia@yeah.net>
2016-04-09 19:42:06 +0800

    fix-single_page-but-more-errors-come-out

--- zhilian/zhilian/spiders/zhaopin.py
+++ zhilian/zhilian/spiders/zhaopin.py
@@ -9,23 +9,23 @@ from scrapy.selector import Selector
 from zhilian.items import ZhilianItem 
 class ZhaopinSpider(scrapy.Spider):
     name = "zhaopin"
-    allowed_domains = ["http://sou.zhaopin.com/jobs/searchresult.ashx"]
+    allowed_domains = ["sou.zhaopin.com"]
     start_urls = (
-        'http://sou.zhaopin.com/jobs/searchresult.ashx',
+        'http://sou.zhaopin.com/jobs/searchresult.ashx?sg=f33d381291364665b40d9bb749ecf9ad&p=1',
     )
     rules = (
-	Rule(LinkExtractor(allow=(r'http://sou.zhaopin.com/jobs/searchresult.ashx?p=\d+')),callback="parse"),
+	Rule(LinkExtractor(allow=(r'http://sou.zhaopin.com/jobs/searchresult.ashx?sg=f33d381291364665b40d9bb749ecf9ad&p=\d')),callback="parse"),
     )
 
     def parse(self, response):
-        '''
-	item = ZhilianItem()
-	item['zwmc'] = response.selector.css('.newlist .zwmc').xpath('//span/text()').extract()
-	item['gsmc'] = response.selector.css('.newlist .gsmc').xpath('text()').extract()
-	item['zwyx'] = response.selector.css('.newlist .zwyx').xpath('text()').extract()
-	item['gzdd'] = response.selector.css('.newlist .gzdd').xpath('text()').extract()
-	yield item
-	'''
+	yield scrapy.Request(response.url,callback=self.parse_content)
+       	next_page = response.css('body > div.main > div.search_newlist_main > div.newlist_main > form > div.clearfix > div.newlist_wrap.fl > div.pagesDown > ul > li.pagesDown-pos > a').xpath('./@href').extract()[0]
+	print next_page
+	if next_page:
+	    yield scrapy.Request(next_page,callback=self.parse)
+    
+    def parse_content(self, response):
+	self.log('crawl_this_url: %s' % response.url)
 	items=[]
 	sel = Selector(response)
 	sites_even = sel.css('#newlist_list_content_table > table')
@@ -37,5 +37,5 @@ class ZhaopinSpider(scrapy.Spider):
 	    item['gzdd'] = sites_even[i].css('tr:nth-child(1)>td.gzdd').xpath('text()').extract()
 	    item['gxsj'] = sites_even[i].css('tr:nth-child(1)>td.gxsj>span').xpath('text()').extract()
 	    items.append(item)
-	return items
+	yield items
 	
